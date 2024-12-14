e74ca9979ae6147fbe8103951f87e0a83622a51c
xedter20 <dextermiranda441@gmail.com>
2024-12-14 18:43:07 +0800

    updates

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,7 +3,7 @@ export const ROUTE_ABOUT = '/cemetery/about';
 export const ROUTE_CONTACT_US = '/cemetery/contact-us';
 export const ROUTE_MAP_VIEW = '/cemetery/map-view';
 export const ROUTE_REGISTRATION = '/cemetery/registration';
-export const ROUTE_LOGIN = '/cemetery/login';
+export const ROUTE_LOGIN = '/';
 
 export const ROUTE_ADMIN_LOGIN = '/cemetery/admin/login';
 export const ROUTE_ADMIN_MAP_SETTING = '/cemetery/admin/map-setting';
