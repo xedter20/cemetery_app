38247966b6e86fa7924c383571bf7343dd791e3b
xedter20 <dextermiranda441@gmail.com>
2024-12-15 18:43:56 +0800

    updates

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,4 +1,6 @@
 export const apiConfig = {
-  baseUrl: "http://localhost:3002/",
-  headers: {},
+  // baseUrl: 'http://localhost:3002/',
+  baseUrl: 'https://cemetery-app-backend-cemetery-service.onrender.com/',
+  // baseUrl: 'https://cemetery-app-backend.onrender.com/',
+  headers: {}
 };
